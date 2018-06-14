88c0d18beef23af7025fed59fd831e59efa8e2f8
unknown <jinmy>
2018-06-14 17:08:56 -0430

    Modificar login

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,9 +49,9 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6|confirmed',
+            
+            'username' => 'required|string|max:255|unique:users',
+            'password' => 'required|string|min:6',
         ]);
     }
 
@@ -64,9 +64,9 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+           
+            'username' => $data['username'],
+            'password' =>$data['password'],
         ]);
     }
 }
